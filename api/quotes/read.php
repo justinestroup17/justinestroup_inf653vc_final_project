d7a62969921f00abffd5bc9f0c5cf3669a91729f
Justine Stroup <jjstroup@mail.fhsu.edu>
2021-05-06 06:04:24 -0600

    First commit GET Quote Request

---
+++ api/quotes/read.php
@@ -0,0 +1,52 @@
+<?php 
+  // Headers
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+
+  include_once '../../config/Database.php';
+  include_once '../../models/Quote.php';
+
+  // Instantiate DB & connect
+  $database = new Database();
+  $db = $database->connect();
+
+  // Instantiate quote object
+  $quote = new Quote($db);
+
+  // Quote query
+  $result = $quote->read();
+  // Get row count
+  $num = $result->rowCount();
+
+  // Check if any quotes
+  if($num > 0) {
+    // Quote array
+    $quotes_arr = array();
+    $quotes_arr['data'] = array();
+
+    while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+      extract($row);
+
+      $quote_item = array(
+        'id' => $id,
+        'quote' => html_entity_decode($quote),
+        'author' => $author,
+        'categoryId' => $categoryId,
+        'authorId' => $authorId,
+        'categoryName' => $categoryName
+      );
+
+      // Push to "data"
+      //array_push($quotes_arr, $quote_item);
+      array_push($quotes_arr['data'], $quote_item);
+    }
+
+    // Turn to JSON & output
+    echo json_encode($quotes_arr);
+
+  } else {
+    // No Posts
+    echo json_encode(
+      array('message' => 'No Posts Found')
+    );
+  }
