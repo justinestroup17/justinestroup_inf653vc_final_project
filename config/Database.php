ac0cb56060737f7b4e7f338177c7d91bdb780040
Justine Stroup <jjstroup@mail.fhsu.edu>
2021-05-10 15:20:14 -0600

    Attempt to connect to heroku

--- config/Database.php
+++ config/Database.php
@@ -1,16 +1,16 @@
 <?php 
   class Database {
     // DB Params
-    private $host = 'localhost';
+    /*private $host = 'localhost';
     private $db_name = 'quotesdb';
     private $username = 'root';
-    private $password = '';
+    private $password = '';*/
     private $conn;
 
     // DB Connect
     public function connect() {
-      $this->conn = null;
-      /*($url = getenv('JAWSDB_MARIA_URL');
+      /*$this->conn = null; */
+      $url = getenv('JAWSDB_MARIA_URL');
       $dbparts = parse_url($url);
 
       $hostname = $dbparts['host'];
@@ -19,12 +19,12 @@
       $database = ltrim($dbparts['path'],'/');
 
       $dsn = "mysql:host={$hostname};dbname={$database}";
-      $this->conn = null; */
+      $this->conn = null;
 
       try { 
-        //$this->conn = new PDO($dsn, $username, $password);
-        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname='. $this->db_name, $this->username, $this->password);
-        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->conn = new PDO($dsn, $username, $password);
+        /*$this->conn = new PDO('mysql:host=' . $this->host . ';dbname='. $this->db_name, $this->username, $this->password);
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); */
       } catch(PDOException $e) {
         echo 'Connection Error: ' . $e->getMessage();
       }
