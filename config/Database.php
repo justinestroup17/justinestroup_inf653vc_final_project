e960d99f783a8b3dc9bfbc59ca8d1bb642a713f7
Justine Stroup <jjstroup@mail.fhsu.edu>
2021-05-10 14:02:38 -0600

    Connection Change

--- config/Database.php
+++ config/Database.php
@@ -1,15 +1,10 @@
 <?php 
   class Database {
     // DB Params
-    //private $host = 'wcwimj6zu5aaddlj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-    //private $db_name = 'quotesdb';
-    //private $username = 'u8q7lyfw1vlm8io2';
-    //private $password = 'tn6uqo7avcuhavc9';
     private $conn;
 
     // DB Connect
     public function connect() {
-      $this->conn = null;
       $url = getenv('JAWSDB_URL');
       $dbparts = parse_url($url);
 
@@ -19,11 +14,10 @@
       $database = ltrim($dbparts['path'],'/');
 
       $dsn = "mysql:host={$hostname};dbname={$database}";
+      $this->conn = null;
 
       try { 
         $this->conn = new PDO($dsn, $username, $password);
-        //$this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
-        //$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       } catch(PDOException $e) {
         echo 'Connection Error: ' . $e->getMessage();
       }
