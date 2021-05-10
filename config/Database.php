69bf332a0aa7335231ea529cba735d9bd03a4929
Justine Stroup <jjstroup@mail.fhsu.edu>
2021-05-10 14:12:37 -0600

    Back

--- config/Database.php
+++ config/Database.php
@@ -13,12 +13,7 @@
       $password = $dbparts['pass'];
       $database = ltrim($dbparts['path'],'/');
 
-      // host: wcwimj6zu5aaddlj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	
-      // username: u8q7lyfw1vlm8io2
-      // password: tn6uqo7avcuhavc9	
-
-      //$dsn = "mysql:host={$hostname};dbname={$database}";
-      $dsn = "mysql:host=wcwimj6zu5aaddlj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=v38z3coezpqellzm";
+      $dsn = "mysql:host={$hostname};dbname={$database}";
       $this->conn = null;
 
       try { 
