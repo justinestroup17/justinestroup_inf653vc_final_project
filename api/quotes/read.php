525775560f3e03b088a34fa7b36e7a8348b4e11a
Justine Stroup <jjstroup@mail.fhsu.edu>
2021-05-10 16:07:02 -0600

    Removed category id author id from quotes/read.php

--- api/quotes/read.php
+++ api/quotes/read.php
@@ -31,8 +31,8 @@
         'id' => $id,
         'quote' => html_entity_decode($quote),
         'author' => $author,
-        'categoryId' => $categoryId,
-        'authorId' => $authorId,
+        //'categoryId' => $categoryId
+        //'authorId' => $authorId,
         'categoryName' => $categoryName
       );
 
